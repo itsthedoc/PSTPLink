1e69a3839d5597e2d9c9a1915691073877e7bb24
Marcus Daniels <itsthedoc@users.noreply.github.com>
2017-12-16 07:45:53 -0500

    Mved Debugging Command
    Moved Convert-ByteArrayToHexString to private function

---
+++ Private/Convert-ByteArrayToHexString.ps1
@@ -0,0 +1,70 @@
+﻿ 
+##############################################################################################################
+#
+# Useful for Debugging to see what the Hex representation is of the Byte Array - Not used in the program
+#
+# Commandline : Convert-ByteArrayToHexString -ByteArray $(Encode-ForTPlink -Body $Body)
+#
+##############################################################################################################
+
+function Convert-ByteArrayToHexString
+{
+################################################################
+#.Synopsis
+# Returns a hex representation of a System.Byte[] array as
+# one or more strings. Hex format can be changed.
+#.Parameter ByteArray
+# System.Byte[] array of bytes to put into the file. If you
+# pipe this array in, you must pipe the [Ref] to the array.
+# Also accepts a single Byte object instead of Byte[].
+#.Parameter Width
+# Number of hex characters per line of output.
+#.Parameter Delimiter
+# How each pair of hex characters (each byte of input) will be
+# delimited from the next pair in the output. The default
+# looks like "0x41,0xFF,0xB9" but you could specify "\x" if
+# you want the output like "\x41\xFF\xB9" instead. You do
+# not have to worry about an extra comma, semicolon, colon
+# or tab appearing before each line of output. The default
+# value is ",0x".
+#.Parameter Prepend
+# An optional string you can prepend to each line of hex
+# output, perhaps like '$x += ' to paste into another
+# script, hence the single quotes.
+#.Parameter AddQuotes
+# A switch which will enclose each line in double-quotes.
+#.Example
+# [Byte[]] $x = 0x41,0x42,0x43,0x44
+# Convert-ByteArrayToHexString $x
+#
+# 0x41,0x42,0x43,0x44
+#.Example
+# [Byte[]] $x = 0x41,0x42,0x43,0x44
+# Convert-ByteArrayToHexString $x -width 2 -delimiter "\x" -addquotes
+#
+# "\x41\x42"
+# "\x43\x44"
+################################################################
+[CmdletBinding()] Param (
+[Parameter(Mandatory = $True, ValueFromPipeline = $True)] [System.Byte[]] $ByteArray,
+[Parameter()] [Int] $Width = 100,
+[Parameter()] [String] $Delimiter = ",0x",
+[Parameter()] [String] $Prepend = "",
+[Parameter()] [Switch] $AddQuotes )
+ 
+if ($Width -lt 1) { $Width = 1 }
+if ($ByteArray.Length -eq 0) { Return }
+$FirstDelimiter = $Delimiter -Replace "^[\,\:\t]",""
+$From = 0
+$To = $Width - 1
+Do
+{
+$String = [System.BitConverter]::ToString($ByteArray[$From..$To])
+$String = $FirstDelimiter + ($String -replace "\-",$Delimiter)
+if ($AddQuotes) { $String = '"' + $String + '"' }
+if ($Prepend -ne "") { $String = $Prepend + $String }
+$String
+$From += $Width
+$To += $Width
+} While ($From -lt $ByteArray.Length)
+}
